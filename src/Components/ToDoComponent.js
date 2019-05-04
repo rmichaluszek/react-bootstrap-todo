9e59abc2d7911d9aea50ae2d7c1a918c234e98e8
Vunnysher <vunnysher@gmail.com>
2019-05-04 12:36:02 +0100

    Theme colors change and items scrollbar

--- src/Components/ToDoComponent.js
+++ src/Components/ToDoComponent.js
@@ -6,7 +6,7 @@ function ToDoComponent() {
   return (
     <div className="ToDoComponent">
 
-      <Card className="Panel" bg="primary" style={{ width: '26rem' }}>
+      <Card className="Panel" bg="primary">
         <Card.Body block>
           <Card.Title>5 / 12 Done</Card.Title>
           
